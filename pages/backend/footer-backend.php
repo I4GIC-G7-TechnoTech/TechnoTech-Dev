972f3f6af729fe3fd22f97d4b71fc4ddc7dd8b1d
S-Mardii <s.mardy009@gmail.com>
2017-05-29 00:55:49 +0700

    add backend package

---
+++ pages/backend/footer-backend.php
@@ -0,0 +1,6 @@
+		<footer>
+			<script type="text/javascript" src="./../../public/vendors/jquery/jquery-3.1.1.min.js"></script>
+			<script type="text/javascript" src="./../../public/vendors/bootstrap/js/bootstrap.min.js"></script>
+		</footer>
+	</body>
+</html>
---
+++ pages/backend/header-backend.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title>Dashboard</title>
+
+		<link rel="stylesheet" type="text/css" href="./../../public/vendors/bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="./../../public/vendors/font-awesome/css/font-awesome.min.css">
+		<link rel="stylesheet" type="text/css" href="./../../public/css/tech.css">
+	</head>
+
+	<body>
+

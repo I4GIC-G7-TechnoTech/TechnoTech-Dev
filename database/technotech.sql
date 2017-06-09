8d7f973551ccceb40ef996ae8ed4318554f4576d
S-Mardii <s.mardy009@gmail.com>
2017-06-09 21:30:58 +0700

    update new database structure

--- database/technotech.sql
+++ database/technotech.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.14
--- http://www.phpmyadmin.net
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 26, 2017 at 11:01 AM
--- Server version: 5.6.17
--- PHP Version: 5.5.12
+-- Generation Time: Jun 09, 2017 at 02:27 PM
+-- Server version: 5.7.14
+-- PHP Version: 5.6.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `technotech`
@@ -26,13 +26,14 @@ SET time_zone = "+00:00";
 -- Table structure for table `camera`
 --
 
-CREATE TABLE IF NOT EXISTS `camera` (
-  `id_camera` int(11) NOT NULL AUTO_INCREMENT,
-  `title_camera` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
-  `content_camera` text COLLATE utf8_unicode_ci NOT NULL,
-  `created_camera` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id_camera`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+CREATE TABLE `camera` (
+  `id` int(11) NOT NULL,
+  `title` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `content` text COLLATE utf8_unicode_ci NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `deleted` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -40,13 +41,14 @@ CREATE TABLE IF NOT EXISTS `camera` (
 -- Table structure for table `computer`
 --
 
-CREATE TABLE IF NOT EXISTS `computer` (
-  `id_computer` int(11) NOT NULL AUTO_INCREMENT,
-  `title_computer` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
-  `content_computer` text COLLATE utf8_unicode_ci NOT NULL,
-  `created_computer` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id_computer`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+CREATE TABLE `computer` (
+  `id` int(11) NOT NULL,
+  `title` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `content` text COLLATE utf8_unicode_ci NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `deleted` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -54,13 +56,27 @@ CREATE TABLE IF NOT EXISTS `computer` (
 -- Table structure for table `news`
 --
 
-CREATE TABLE IF NOT EXISTS `news` (
-  `id_news` int(11) NOT NULL AUTO_INCREMENT,
-  `title_news` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
-  `content_news` text COLLATE utf8_unicode_ci NOT NULL,
-  `created_news` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id_news`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+CREATE TABLE `news` (
+  `id` int(11) NOT NULL,
+  `title` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `content` text COLLATE utf8_unicode_ci NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `deleted` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `news`
+--
+
+INSERT INTO `news` (`id`, `title`, `content`, `created`, `updated`, `deleted`) VALUES
+(1, 'Test', '<p>Hi</p>', '2017-06-09 01:43:23', '2017-06-09 08:43:23', '2017-06-09 01:43:23'),
+(2, 'Hi', '<p>Test</p>', '2017-06-09 01:44:03', '2017-06-09 08:44:03', '2017-06-09 01:44:03'),
+(3, 'Cool', '<p>Test</p>', '2017-06-09 01:46:23', '2017-06-09 08:46:23', '2017-06-09 01:46:23'),
+(4, 'Lorem', '<p><strong>Test</strong></p>\r\n<p>&nbsp;</p>', '2017-06-09 03:12:12', '2017-06-09 10:12:12', '2017-06-09 03:12:12'),
+(5, '', '<p><strong>Lorem Ipsum</strong> is simply dummy text of the printing and typesetting industry. Loremever</p>', '2017-06-09 03:16:51', '2017-06-09 10:16:51', '2017-06-09 03:16:51'),
+(6, 'Help', '<p>Test</p>', '2017-06-09 03:20:51', '2017-06-09 10:20:51', '2017-06-09 03:20:51'),
+(7, 'Test', '<p>Testtsts</p>', '2017-06-09 04:47:41', '2017-06-09 11:47:41', '2017-06-09 04:47:41');
 
 -- --------------------------------------------------------
 
@@ -68,13 +84,14 @@ CREATE TABLE IF NOT EXISTS `news` (
 -- Table structure for table `phone`
 --
 
-CREATE TABLE IF NOT EXISTS `phone` (
-  `id_phone` int(11) NOT NULL AUTO_INCREMENT,
-  `title_phone` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
-  `content_phone` text COLLATE utf8_unicode_ci NOT NULL,
-  `created_phone` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id_phone`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+CREATE TABLE `phone` (
+  `id` int(11) NOT NULL,
+  `title` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `content` text COLLATE utf8_unicode_ci NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `deleted` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -82,14 +99,16 @@ CREATE TABLE IF NOT EXISTS `phone` (
 -- Table structure for table `user`
 --
 
-CREATE TABLE IF NOT EXISTS `user` (
-  `id_user` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
   `fullname` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
   `username` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
   `password` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
   `profilePic` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id_user`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated` datetime DEFAULT CURRENT_TIMESTAMP,
+  `deleted` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -97,14 +116,89 @@ CREATE TABLE IF NOT EXISTS `user` (
 -- Table structure for table `wiki`
 --
 
-CREATE TABLE IF NOT EXISTS `wiki` (
-  `id_wiki` int(11) NOT NULL AUTO_INCREMENT,
-  `title_wiki` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
-  `content_wiki` text COLLATE utf8_unicode_ci NOT NULL,
-  `created_wiki` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id_wiki`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+CREATE TABLE `wiki` (
+  `id` int(11) NOT NULL,
+  `title` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `content` text COLLATE utf8_unicode_ci NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `deleted` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `camera`
+--
+ALTER TABLE `camera`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `computer`
+--
+ALTER TABLE `computer`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `news`
+--
+ALTER TABLE `news`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `phone`
+--
+ALTER TABLE `phone`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `wiki`
+--
+ALTER TABLE `wiki`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `camera`
+--
+ALTER TABLE `camera`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `computer`
+--
+ALTER TABLE `computer`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `news`
+--
+ALTER TABLE `news`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+--
+-- AUTO_INCREMENT for table `phone`
+--
+ALTER TABLE `phone`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `wiki`
+--
+ALTER TABLE `wiki`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

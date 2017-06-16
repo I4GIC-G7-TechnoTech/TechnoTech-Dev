97e5ea5b03d089b3cc743799fa6df94269ca5ce7
Prasal Soeurn <Prasal Soeurn>
2017-06-16 14:42:17 +0700

    add archive and single camera

---
+++ pages/single-camera-page-2.php
@@ -0,0 +1,146 @@
+<?php 	
+	require_once('header.php');
+?>
+<div class="archive-title">
+            <h1>Canon EOS Rebel T7i</h1>
+            <strong>Author: <em>Peire PETTER</em></strong>
+            <hr class="main-title">
+        </div>
+      
+        <div class="line">
+            <h4><b>Features Optical Viewfinder with a 45-point All Cross-type AF System</b></h4>
+            <br>
+            <div class="col-xs-12 col-md-4">
+                <img class="img-thumbnail" src="public\img\singlepage1.jpg">
+            </div>
+
+            <div class="col-xs-12 col-md-8">
+                <p>
+                    Take your photos to the next level with the image quality and near-instant autofocus of the EOS Rebel T7i camera. A 45-point all cross-type AF system* and optical viewfinder lets you stay ready for that unforgettable moment with virtually no lag between what you see and what you get when you press the shutter. Fast and accurate Dual Pixel CMOS AF with phase-detection locks focus quickly and accurately to help make sure you don’t miss a great shot. A 24.2 Megapixel CMOS (APS-C) sensor helps you capture special moments from vacations to family events in brilliant color, detail and vibrancy for beautifully memorable photos and videos. With a maximum ISO of 25600, you can capture photos in low light with minimal chance of blur. Built-in Wi-Fi®**, NFC*** and Bluetooth®^ connectivity lets you share these precious moments with friends and family and upload them directly to the web.
+                </p>
+            </div>
+            <br>
+        </div>
+        <div class="clearfix"></div>
+        <div class="line">
+            <h4><b>24.2 Megapixel CMOS (APS-C) Sensor</b></h4>
+            <div class="col-xs-12 col-md-8">
+                <p>
+                    Among all the interchangeable lens digital cameras incorporating the APS-C size image sensors with phase-difference detection AF on the image plane. As of February 14, 2017 (based on Canon research). Calculated based on the resulting AF speed measured according to the CIPA guidelines. (Varies depending on the shooting conditions and the lenses used.) Internal measurement method.
+                </p>
+            </div>
+            <div class="col-xs-12 col-md-4">
+                <img class="img-thumbnail" src="public\img\camerapage1_2.jpg">
+            </div>
+        </div>
+        <br>
+        <div class="line">
+            <h4><b>DIGIC 7 Image Processor, ISO 100-25600</b></h4>
+            <div class="col-xs-12 col-md-4">
+                <img class="img-thumbnail" src="public\img\camerapage1_3.jpg">
+            </div>
+            <div class="col-xs-12 col-md-8">
+                <p>
+                    The DIGIC 7 Image Processor powers the EOS Rebel T7i camera to produce high image quality and fast operation, even in in low light. When using high ISO settings, the image processing helps keep results sharp and detailed in virtually any lighting situation. Powerful all around, the DIGIC 7 Image Processor helps ensure your photos and videos look sharp and lifelike with minimal unwanted noise or grain.
+                </p>
+                <p>
+                    The EOS Rebel T7i camera is designed to make Wi-Fi® connections** fast and easy. It can exchange data with other Wi-Fi® compatible Canon cameras, and transfer files directly to a compatible smart device using the Camera Connect app.
+                </p>
+            </div>
+            <div class="clearfix"></div>
+            <br>
+        </div>
+
+        <div class="line">
+            <h4><b>Vari-angle Touch Screen, 3.0-inch LCD</b></h4>
+            <div class="col-xs-12 col-md-8">
+                <p>
+                    The XC10 is perhaps the least-conventional model on this list, but it’s a viable alternative to the company’s other, pricier, Cinema EOS models. Designed for video but with high-quality stills in mind too, the model blends a 12MP 1-inch CMOS sensor with a 24.1-241mm image-stabilised lens, and packages it in a relatively compact body whose adjustable grip provides excellent flexibility. It records 4K UHD footage at 8-bit 4:2:2 footage internally, and you can call upon the Canon Log option to capture up to 12EV stops of dynamic range. You also get a built-in ND filter, clean HDMI output and focus peaking, and Canon even throws in a loupe so that you can use the rear LCD touchscreen much like an electronic viewfinder. With a smaller sensor than more traditional options, and at a similar price point, it may not be the best option for low-light shooting, but when you consider the zoom lens, form factor and value for money, it scores lots of points. The model was recently superseded by the XC15
+                </p>
+            </div>
+            <div class="col-xs-12 col-md-4">
+                <img class="img-thumbnail" src="public\img\camerapage1_5.jpg">
+            </div>
+            <div class="clearfix"></div>
+            <br>
+        </div>
+            
+        <div class="line">
+            <h4><b>Movie Electronic IS</b></h4>
+            <div class="col-xs-12 col-md-4">
+                <img class="img-thumbnail" src="public\img\camerapage1_6.jpg">
+            </div>
+            <div class="col-xs-12 col-md-8">
+                <p>
+                    A shaky video can make even the best content hard to watch. The EOS Rebel T7i camera features Movie Electronic IS^^^ to help minimize the blurring effect of camera shake. When enabled and used with a compatible Canon lens, Movie Electronic IS adds 5-axis stabilization in the horizontal, vertical and rotational planes. The result? Smooth Full HD movies for your friends and family to enjoy. 
+                </p>
+                <p>
+                    Movie Electronic IS cannot be used with the following lenses: EF 50mm F1.2L USM, EF 85mm F1.2L II USM, EF 200mm F2L IS USM, EF 300mm F2.8L IS II USM, EF 400mm F2.8L IS II USM, EF 500mm F4L IS II USM, EF 600mm F4L IS II USM, EF 800mm F5.6L IS USM, EF 200–400mm F4L IS USM.</p>
+                </p>
+            </div>
+        </div>
+        <div class="clearfix"></div>
+        <div class="line">
+            <!-- embedded-video -->
+            <div class="embed-responsive embed-responsive-16by9 video-wiki">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/VIR2aN-3r7A " frameborder="0" allowfullscreen></iframe>
+            </div>
+            <!-- End embedded-video -->
+        </div>
+        
+        <!-- related-post -->
+        <div class="related-post">
+        <br/>
+            <h3>Related Posts</h3>
+            <hr>
+            <div class="col-sm-6 col-md-3">
+                <div class="thumbnail">
+                    <a href="single-camera-page-1.php">
+                        <img src="public\img\1.png" alt="Related Post">
+                        <div class="caption">
+                            <h4 class="related-title">5 Best 4K Cameras in 2017</h4>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3">
+                <div class="thumbnail">
+                    <a href="#">
+                        <img src="public/img/3.jpg" alt="Related Post">
+                        <div class="caption">
+                            <h4 class="related-title">New 18-55mm kit lens</h4>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3">
+                <div class="thumbnail">
+                    <a href="#">
+                        <img src="public/img/4.jpg" alt="Related Post">
+                        <div class="caption">
+                            <h4 class="related-title">Canon - EOS Rebel SL1</h4>
+                           
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3">
+                <div class="thumbnail">
+                    <a href="#">
+                        <img src="public/img/5.jpg" alt="Related Post">
+                        <div class="caption">
+                            <h4 class="related-title">Canon-EOS 5D Mark IV</h4>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+        <!-- END related-post  -->
+	</div>
+	<!-- End top-container -->
+
+
+
+
+<?php require_once('footer.php') ?>
